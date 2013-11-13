554ed1e8b30eaf3fe8a80974099996fb50163907
p-slice <p.slice169@gmail.com>
2013-11-12 22:57:37 -0800

    Added use of brackets in equations

--- bot/net/pslice/bot/commands/CommandSolve.java
+++ bot/net/pslice/bot/commands/CommandSolve.java
@@ -1,12 +1,13 @@
 package net.pslice.bot.commands;
 
 import net.pslice.bot.AMP;
-import net.pslice.bot.source.BotCommand;
 import org.pircbotx.Channel;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class CommandSolve {
@@ -15,26 +16,36 @@ class CommandSolve {
 
     public static void execute(Channel chan, User user, String[] messageSplit, int p, int rank) {
 
-        List<String> list = new ArrayList();
-        list.add("+solve");
-        int x = 1;
-        if (messageSplit[1].equals("quadratic")){
-            x = 2;
-            list.add("quadratic");
+        String[] equationSplit;
+
+        List<String> list = new ArrayList<>();
+
+        if (messageSplit[messageSplit.length - 1].equals("+s"))
+            list.addAll(Arrays.asList(messageSplit).subList(0, messageSplit.length - 1));
+        else {
+            list.add("+solve");
+            int x = 1;
+            if (messageSplit[1].equals("quadratic")) {
+                x = 2;
+                list.add("quadratic");
+            }
+            String equation = "";
+            for (int z = x; z < messageSplit.length; z++) {
+                equation += messageSplit[z];
+            }
+            equationSplit = equation.split("((?<=[+,\\-,*,/,^,a-z,A-Z,(,)])|(?=[+,\\-,*,/,^,a-z,A_Z,(,)]))");
+            Collections.addAll(list, equationSplit);
+            for (int r = 0; r < list.size(); r++){
+                if (list.get(r).equals(""))
+                    list.remove(r);
+            }
         }
-        String equation = "";
-        for (int z = x; z < messageSplit.length; z++){
-            equation += messageSplit[z];
-        }
-        String[] equationSplit = equation.split("((?<=[+,\\-,*,/,^,x])|(?=[+,\\-,*,/,^,x]))");
-        for (int g = 0; g < equationSplit.length; g++)
-            list.add(equationSplit[g]);
         equationSplit = list.toArray(new String[list.size()]);
 
         int l = equationSplit.length;
 
         if (p >= rank) {
-            if (l == 11 && equationSplit[1].toLowerCase().equals("quadratic")){
+            if (l == 11 && equationSplit[1].toLowerCase().equals("quadratic")) {
 
                 if (!equationSplit[2].equals("0")
                         && equationSplit[3].equals("x")
@@ -42,89 +53,76 @@ class CommandSolve {
                         && equationSplit[5].equals("2")
                         && equationSplit[6].matches("[+,-]+")
                         && equationSplit[8].equals("x")
-                        && equationSplit[9].matches("[+,-]+")){
+                        && equationSplit[9].matches("[+,-]+")) {
                     String result = solveQuadratic(equationSplit);
                     bot.sendMessage(chan, result);
-                }
-                else
-                    bot.sendMessage(chan, "Error - equation is not quadratic!");
-            }
-            else if (l >= 4 && !equationSplit[1].toLowerCase().equals("quadratic")) {
-                for (int i = 0; i < equationSplit.length; i ++){
-                    if (equationSplit[i].equals("/") && equationSplit[i+1].matches("[-?0]+")){
-                        bot.sendMessage(chan, "Error - equation contains division by 0!");
+                } else
+                    bot.sendMessage(chan, "Error: equation is not quadratic!");
+            } else if (l >= 4 && !equationSplit[1].toLowerCase().equals("quadratic")) {
+                for (int i = 0; i < equationSplit.length; i++) {
+                    if (equationSplit[i].equals("/") && equationSplit[i + 1].matches("[-?0]+")) {
+                        bot.sendMessage(chan, "Error: equation contains division by 0!");
                         return;
                     }
                 }
-                double result = solveFullEquation(equationSplit);
-                bot.sendMessage(chan, "Result: " + String.valueOf(result).replaceAll("\\.0\\b", ""));
+                String result = solveFullEquation(equationSplit);
+                bot.sendMessage(chan, result);
             } else
-                Command.throwIncorrectParametersError(user, "+solve (quadratic) <equation>");
+                Command.throwIncorrectParametersError(user, "+solve (quadratic) <equation> (+s)");
         } else
             Command.throwNoRankError(user, rank, p);
     }
 
-    private static double solveFullEquation(String[] equation){
+    private static String solveFullEquation(String[] equation) {
+
+        List<Double> brNumList = new ArrayList<>();
+        List<String>  brOpList = new ArrayList<>();
+
         List<Double> numList = new ArrayList<>();
         List<String> opList = new ArrayList<>();
 
-        for (int i = 1; i < equation.length; i++){
+        for (int i = 1; i < equation.length; i++) {
+            if (equation[i].equals("(")){
+                int f = 0;
+                for (int q = i; q < equation.length; q++){
+                    if (equation[q].equals("(") && q != i)
+                        return "Error: cannot compute stacked brackets at this time!";
+                    else if (equation[q].matches("[+,\\-,*,/,^]+"))
+                        brOpList.add(equation[q]);
+                    else if (equation[q].matches("-?\\d+"))
+                        brNumList.add(Double.parseDouble(equation[q]));
+                    else if (equation[q].equals(")")){
+                        f = q;
+                        break;
+                    }
+                }
+                double bracket = getResult(brNumList, brOpList);
+
+                numList.add(bracket);
+                if (!equation[i-1].matches("[+,\\-,*,/,^,+solve,)]+"))
+                    opList.add("*");
+                if (f+1 != equation.length){
+                    if (!equation[f+1].matches("[+,\\-,*,/,^,+s]+"))
+                        opList.add("*");
+                }
+                brNumList.clear();
+                brOpList.clear();
+                i = f;
+            }
             if (equation[i].matches("[+,\\-,*,/,^]+"))
                 opList.add(equation[i]);
             else if (equation[i].matches("-?\\d+"))
                 numList.add(Double.parseDouble(equation[i]));
+            else if (equation[i].matches("-?\\[a-z,A-Z]+"))
+                return "Error: cannot currently work with variables!";
         }
 
-        while (numList.size() > 1){
-            for (int l = 0; l < opList.size(); l++){
-                double first = numList.get(l);
-                double second = numList.get(l + 1);
+        double result = getResult(numList, opList);
 
-                String op = opList.get(l);
-
-                boolean didEquation = false;
-
-                if (op.equals("^")) {
-                    first = Math.pow(first, second);
-                    didEquation = true;
-                }
-                else if (op.equals("/")
-                        && !opList.contains("^")) {
-                    first /= second;
-                    didEquation = true;
-                }
-                else if (op.equals("*")
-                        && !opList.contains("^")) {
-                    first *= second;
-                    didEquation = true;
-                }
-                else if (op.equals("+")
-                        && !opList.contains("^")
-                        && !opList.contains("*")
-                        && !opList.contains("/")) {
-                    first += second;
-                    didEquation = true;
-                }
-                else if (op.equals("-")
-                        && !opList.contains("^")
-                        && !opList.contains("*")
-                        && !opList.contains("/"))  {
-                    first -= second;
-                    didEquation = true;
-                }
-
-                if (didEquation){
-                    numList.remove(l + 1);
-                    numList.remove(l);
-                    numList.add(l, first);
-                    opList.remove(l);
-                }
-            }
-        }
-        return numList.get(0);
+        return "Result: " + String.valueOf(result).replaceAll("\\.0\\b", "");
     }
 
-    private static String solveQuadratic(String[] equation){
+    private static String solveQuadratic(String[] equation) {
         String result;
 
         double a = Double.parseDouble(equation[2]);
@@ -141,7 +139,7 @@ class CommandSolve {
 
         double part1 = Math.pow(b, 2) - (4 * a * c);
         if (part1 < 0)
-            result = "Error - situation is impossible!";
+            result = "Error: situation is impossible!";
         else {
             result1 = (-b + Math.sqrt(part1)) / (2 * a);
             result2 = (-b - Math.sqrt(part1)) / (2 * a);
@@ -161,5 +159,53 @@ class CommandSolve {
         }
 
         return result;
+    }
+
+    private static double getResult(List<Double> numList, List<String> opList){
+        if (numList.size() == 0)
+            numList.add((double) 1);
+        while (numList.size() > 1) {
+            for (int l = 0; l < opList.size(); l++) {
+                double first = numList.get(l);
+                double second = numList.get(l + 1);
+
+                String op = opList.get(l);
+
+                boolean didEquation = false;
+
+                if (op.equals("^")) {
+                    first = Math.pow(first, second);
+                    didEquation = true;
+                } else if (op.equals("/")
+                        && !opList.contains("^")) {
+                    first /= second;
+                    didEquation = true;
+                } else if (op.equals("*")
+                        && !opList.contains("^")) {
+                    first *= second;
+                    didEquation = true;
+                } else if (op.equals("+")
+                        && !opList.contains("^")
+                        && !opList.contains("*")
+                        && !opList.contains("/")) {
+                    first += second;
+                    didEquation = true;
+                } else if (op.equals("-")
+                        && !opList.contains("^")
+                        && !opList.contains("*")
+                        && !opList.contains("/")) {
+                    first -= second;
+                    didEquation = true;
+                }
+
+                if (didEquation) {
+                    numList.remove(l + 1);
+                    numList.remove(l);
+                    numList.add(l, first);
+                    opList.remove(l);
+                }
+            }
+        }
+        return numList.get(0);
     }
 }
